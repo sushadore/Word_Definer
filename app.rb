8654025a005a2e490a8be14980d7cbc966a3bfe5
Susha Dore <susha.dore@gmail.com>
2017-04-28 17:41:29 -0700

    return new word list, word input form and results page

--- app.rb
+++ app.rb
@@ -8,25 +8,12 @@ get '/' do
   erb :index
 end
 
-get '/word/new' do
+get '/add_word' do
   erb :word_form
 end
 
-post '/word/new' do
+post '/new_word' do
   @word = Word.new(:new_word => params.fetch('word'))
   @word.save
   erb :word_displayed
-end
-
-get '/definition/:id/word/new' do
-  @word = Word.find(params.fetch('id').to_i)
-  erb :definition_form
-end
-
-post '/definition/new' do
-  @definition = Definition.new(new_definition => params.fetch('definition'))
-  @definition.save
-  @word = Word.find(params.fetch('word_id').to_i)
-  @word.add_definition(@definition)
-
 end
