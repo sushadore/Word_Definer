8460f987b4a70a1b52ad274cf95eddd5b2099f1c
Susha Dore <susha.dore@gmail.com>
2017-05-03 19:36:56 -0700

    finalize functionality of site

--- app.rb
+++ app.rb
@@ -1,43 +1,48 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require './lib/definition'
+require './lib/word'
 also_reload 'lib/**/*.rb'
-require './lib/Word'
-require './lib/Definition'
 
 get '/' do
-  @words = Word.all
   erb :index
 end
 
-get '/word/new' do
-  erb :word_form
+get '/words/new' do
+  erb :words_form
 end
 
-post '/word' do
-  @word = Word.new(:new_word => params.fetch('word'))
+get '/words' do
+  @words = Word.all
+  erb :words
+end
+
+post '/words' do
+  @word = Word.new({:new_word => params.fetch('word')})
   @word.save
+  @words = Word.all
   erb :word_definitions_form
 end
 
-get 'word/:id' do
+# get '/vehicles/:id' do
+#   @vehicle = Vehicle.find(params.fetch('id').to_i)
+#   erb :vehicle
+# end
+
+get '/words/:id' do
   @word = Word.find(params.fetch('id').to_i)
   erb :word
 end
 
-get '/word/:id/definitions/new' do
+get '/words/:id/definitions/new' do
   @word = Word.find(params.fetch('id').to_i)
   erb :word_definitions_form
 end
 
 post '/definitions' do
-  @definition = Definition.new(:new_definition => params.fetch('definition'))
+  @definition = Definition.new({:new_definition => params.fetch('definition')})
   @definition.save
-  @word = Word.find(params.fetch('word_id').to_i)
+  @word = Word.find(params.fetch('word_id').to_i())
   @word.add_definition(@definition)
   erb :word
 end
-
-# get '/definitions' do
-#   @definitions = Definition.all
-#   erb :definitions
-# end
--- lib/word.rb
+++ lib/word.rb
@@ -1,8 +1,8 @@
 class Word
-  attr_reader(:new_word, :id, :definitions)
+  attr_reader(:new_word, :definitions, :id)
   @@all_words = []
 
-  define_method(:initialize) do |attributes|
+  def initialize(attributes)
      @new_word = attributes.fetch(:new_word)
      @definitions = []
      @id = @@all_words.length + 1
@@ -28,5 +28,27 @@ class Word
 
   def add_definition(definition)
     @definitions.push(definition)
+  end
+end
+
+class Definition
+  attr_reader(:new_definition)
+  @@all_definitions = []
+
+  def initialize(attributes)
+    @new_definition = attributes.fetch(:new_definition)
+    @id = @@all_definitions.length + 1
+  end
+
+  def Definition.all
+    @@all_definitions
+  end
+
+  def save
+    @@all_definitions.push(self)
+  end
+
+  def id
+    @id
   end
 end
