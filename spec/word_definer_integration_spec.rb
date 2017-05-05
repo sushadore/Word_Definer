3d70bcb1e32a412363a78a2e802b9170b085f28d
Susha Dore <susha.dore@gmail.com>
2017-05-04 21:16:22 -0700

    pass integration specs

--- spec/word_definer_integration_spec.rb
+++ spec/word_definer_integration_spec.rb
@@ -4,22 +4,20 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe 'the Word-Definer path', {:type => :feature} do
-  it 'it returns a user chosen word along with user definitions' do
+  it 'it returns a inputted user word along with user definitions' do
     visit '/'
     click_link 'Add New Word'
-
     expect(page).to have_content ''
+
     fill_in 'word', :with => 'Loquacity'
     click_button 'Add Word'
-
     expect(page).to have_content 'Loquacity'
-    click_button 'Add definition'
 
-    fill_in 'definition', :with => 'Loquacity'
-    expect(page).to have_content 'Loquacity, Marko'
+    fill_in 'definition', :with => 'Marko'
+    click_button 'Add definition'
+    expect(page).to have_content 'Marko'
 
     click_link 'My words!'
-
     expect(page).to have_content 'Loquacity'
   end
 end
